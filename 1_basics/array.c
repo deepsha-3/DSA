5982bb11244e3cd4e6f7b4168e64d37c9f43e6b1
deepsha-3 <itsmedeepsha375@gmail.com>
2026-01-04 22:43:13 +0545

    new DSA file added

---
+++ 1_basics/array.c
@@ -0,0 +1,14 @@
+
+// Array in C 
+
+#include<stdio.h>
+int main(){
+    int arr[3];
+    arr[0] = 10;
+    arr[1] = 20;
+
+    printf("First element: %d\n", arr[0]);
+    printf("Second element: %d\n", arr[1]);
+
+    return 0;
+}
